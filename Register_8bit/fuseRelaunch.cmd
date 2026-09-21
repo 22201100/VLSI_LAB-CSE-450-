bca81f284ad47ba0718a4ca9c6673fcf88d65107
22201100 <22201100@uap-bd.edu>
2026-09-22 00:03:38 +0600

    Add Register 8bit project

---
+++ Register_8bit/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/ise/VLSI_LAB_EXMPLES/Register_8bit/register_8bit_tb_isim_beh.exe" -prj "/home/ise/VLSI_LAB_EXMPLES/Register_8bit/register_8bit_tb_beh.prj" "work.register_8bit_tb" 
